6684c793433802b1a040fcb6960e0de0913ba676
Shreyas Balachandran <shreyas@shreyas.ultrahub>
2025-02-18 21:23:20 +0000

    Added chi package, refactored code

--- main.go
+++ main.go
@@ -5,20 +5,24 @@ import (
 	"io"
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/hello-world", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Hello world")
 		io.WriteString(w, "Hello world")
 	})
 
-	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Health check")
 		io.WriteString(w, "200 - OK")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
