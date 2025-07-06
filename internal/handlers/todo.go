c17e2b196a4c100810236a6a9dccf4a641b264ff
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2025-07-06 11:13:45 +0100

    feat(todo): added and integrated the in-memory store to the todo handler routes

--- internal/handlers/todo.go
+++ internal/handlers/todo.go
@@ -2,7 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
-	"io"
+	"errors"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -10,10 +10,20 @@ import (
 	"github.com/shreyas1307/go-server/internal/store/models"
 )
 
+// TodoHandler handles requests related to the todo resource.
 type TodoHandler struct {
 	Store store.Store
 }
 
+// todoResponse is the structure of the response for todo-related requests.
+type todoResponse struct {
+	Successful bool          `json:"successful"`
+	Message    string        `json:"message"`
+	Data       []models.Todo `json:"data"`
+	Error      error         `json:"error,omitempty"`
+}
+
+// Router sets up the routes for the TodoHandler.
 func (th *TodoHandler) Router() chi.Router {
 	router := chi.NewRouter()
 	router.Get("/", th.GetTodos)
@@ -21,18 +31,48 @@ func (th *TodoHandler) Router() chi.Router {
 	return router
 }
 
+// NewTodoHandler creates a new TodoHandler with the provided store.
+//
+// It returns an error if the store is nil.
+func NewTodoHandler(store store.Store) (*TodoHandler, error) {
+	if store == nil {
+		return nil, errors.New("store cannot be nil")
+	}
+	return &TodoHandler{
+		Store: store,
+	}, nil
+}
+
+// GetTodos handles the request to fetch all todo items.
 func (th *TodoHandler) GetTodos(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	_, err := th.Store.GetTodos(ctx)
+	todoItems, err := th.Store.GetTodos(ctx)
+	println(todoItems) // Debugging, I'm unable to print the todo items for the response
 	if err != nil {
 		w.WriteHeader(http.StatusNotFound)
-		io.WriteString(w, "Error fetching todos")
+		res, _ := json.Marshal(&todoResponse{
+			Successful: false,
+			Message:    "Error fetching todos",
+			Error:      err,
+		})
+		w.Write(res)
 		return
 	}
 	w.WriteHeader(http.StatusOK)
-	io.WriteString(w, "Get Todos \n")
+	res, err := json.Marshal(&todoResponse{
+		Successful: true,
+		Message:    "Todos fetched successfully",
+		Data:       todoItems,
+	})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
 }
 
+// AddTodo handles the request to add a new todo item.
 func (th *TodoHandler) AddTodo(w http.ResponseWriter, r *http.Request) {
 
 	var todo models.Todo
@@ -40,13 +80,18 @@ func (th *TodoHandler) AddTodo(w http.ResponseWriter, r *http.Request) {
 	decoder.DisallowUnknownFields()
 	if err := decoder.Decode(&todo); err != nil {
 		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, "Error decoding todo \n")
+		res, _ := json.Marshal(&todoResponse{
+			Successful: false,
+			Message:    "Invalid request body",
+			Error:      err,
+		})
+		w.Write(res)
 		return
 	}
 
-	res, err := json.Marshal(map[string]interface{}{
-		"successful": true,
-		"message":    "new todo added",
+	res, err := json.Marshal(&todoResponse{
+		Successful: true,
+		Message:    "new todo added",
 	})
 
 	if err != nil {
--- internal/handlers/todo_test.go
+++ internal/handlers/todo_test.go
@@ -16,12 +16,34 @@ import (
 
 type MockServerStore struct{}
 
+type MockTodoResponse struct {
+	Successful bool          `json:"successful"`
+	Message    string        `json:"message"`
+	Data       []models.Todo `json:"data,omitempty"`
+	Error      error         `json:"error,omitempty"`
+}
+
 func (m *MockServerStore) AddTodo(ctx context.Context, todo models.Todo) error {
 	return nil
 }
 
-func (m *MockServerStore) GetTodos(ctx context.Context) (models.TodoList, error) {
-	return models.TodoList{}, nil
+func (m *MockServerStore) GetTodos(ctx context.Context) ([]models.Todo, error) {
+	return []models.Todo{}, nil
+}
+
+func TestNewTodoHandler(t *testing.T) {
+	store := &MockServerStore{}
+	th, err := handlers.NewTodoHandler(store)
+	assert.NoError(t, err)
+	assert.NotNil(t, th)
+	assert.Equal(t, store, th.Store)
+}
+
+func TestNewHandlerNilStore(t *testing.T) {
+	th, err := handlers.NewTodoHandler(nil)
+	assert.Error(t, err)
+	assert.Nil(t, th)
+	assert.Equal(t, "store cannot be nil", err.Error())
 }
 
 func TestAddTodoSuccessful(t *testing.T) {
@@ -51,9 +73,9 @@ func TestAddTodoSuccessful(t *testing.T) {
 	assert.NoError(t, err)
 	defer res.Body.Close()
 
-	response := map[string]interface{}{
-		"successful": true,
-		"message":    "new todo added",
+	response := &MockTodoResponse{
+		Successful: true,
+		Message:    "new todo added",
 	}
 	responseMarshal, err := json.Marshal(response)
 	assert.NoError(t, err)
@@ -84,4 +106,32 @@ func TestAddTodoFailure(t *testing.T) {
 	// below test fails... may have to edit handler code to
 	// check if I'm getting the corect request body
 	assert.Equal(t, http.StatusBadRequest, res.StatusCode)
+}
+
+func TestGetTodosSuccessful(t *testing.T) {
+	th := &handlers.TodoHandler{
+		Store: &MockServerStore{},
+	}
+
+	rq := httptest.NewRequest(http.MethodGet, "/todo/get", nil)
+	rr := httptest.NewRecorder()
+
+	th.GetTodos(rr, rq)
+
+	res := rr.Result()
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+
+	b, err := io.ReadAll(res.Body)
+	assert.NoError(t, err)
+	defer res.Body.Close()
+
+	response := &MockTodoResponse{
+		Successful: true,
+		Message:    "Todos fetched successfully",
+		Data:       []models.Todo{},
+	}
+	responseMarshal, err := json.Marshal(response)
+	assert.NoError(t, err)
+
+	assert.Equal(t, responseMarshal, b)
 }
