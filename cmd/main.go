14f3034f0abe7d0ded5ef36b9843431b6eb97530
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2025-07-06 11:10:13 +0100

    refactor: refactored main.go

--- cmd/main.go
+++ cmd/main.go
@@ -21,31 +21,47 @@ type Server struct {
 }
 
 func main() {
+	// Setting up the context and logger
 	ctx := context.Background()
-	loglevel := "debug"
+	loglevel := os.Getenv("LOG_LEVEL")
 
-	err := setupLogger(ctx, loglevel)
-	if err != nil {
+	if err := setupLogger(ctx, loglevel); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	// Creatinng a new server instance
 	srv := Server{
 		mux: chi.NewRouter(),
 	}
+
+	// Initialise the in-memory store
+	var memorydb store.Store = store.NewInMemoryStore()
+
+	// Initialising the TodoHandler with the in-memory store
+	todohandler, err := handlers.NewTodoHandler(memorydb)
+	if err != nil {
+		fmt.Printf("Error creating TodoHandler: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Initialising the server middleware and routes
 	srv.mux.Use(middleware.Logger)
 	srv.mux.Use(middleware.Recoverer)
 	srv.mux.Get("/hello-world", HelloWorldHandler)
 
-	srv.mux.Mount("/todo", (&handlers.TodoHandler{
-		Store: srv.db,
-	}).Router())
+	srv.mux.Mount("/todo", todohandler.Router())
 
 	srv.mux.Get("/healthz", HealthzHandler)
 
+	// Serving the application
 	log.Fatal(http.ListenAndServe(":8080", srv.mux))
 }
 
-func setupLogger(ctx context.Context, loglevel string) error {
+// setupLogger initialises the global logger with a specified log level.
+//
+// It returns an error if the log level is invalid.
+func setupLogger(_ context.Context, loglevel string) error {
 	l, err := zerolog.ParseLevel(loglevel)
 	if err != nil {
 		return fmt.Errorf("failed to set default log level: %w", err)
@@ -54,11 +70,13 @@ func setupLogger(ctx context.Context, loglevel string) error {
 	return nil
 }
 
+// HelloWorldHandler is a simple http handler that reponds with "Hello world".
 func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Hello world \n")
 	io.WriteString(w, "Hello world \n")
 }
 
+// HealthzHandler is a health check endpoint that responds with the server's current health status.
 func HealthzHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Health check \n")
 	io.WriteString(w, "200 - OK \n")
